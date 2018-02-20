008eea7d0c74b7c1ed5b3d3fb78ca41837b8fc00
kade killary <kade.killary@xmedia.com>
2018-02-20 11:44:30 -0500

    now with face detection

--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+# Vim wacked out my spacing - sorry!
 library(shiny)
 library(readr)
 library(DT)
@@ -5,23 +6,23 @@ library(shinydashboard)
 library(googleAuthR)
 library(RoogleVision)
 library(shinycssloaders)
-library(DT)
 
+# Authenticating to Google
+# Have to get credentials for Google Developer Console
 
-CLIENT.ID  <- "ENTER YOUR CLIENT ID"
-CLIENT.SECRET  <- "ENTER YOUR CLIENT SECRET"
-SERVICE.ACCOUNT  <- "PATH TO SERVICE ACCOUNT.json"
+CLIENT_ID  <- ""
+CLIENT_SECRET  <- ""
+PATH_TO_SERVICE  <- ""
 
-
-options("googleAuthR.webapp.client_id" = CLIENT.ID)
-options("googleAuthR.webapp.client_secret" = CLIENT.SECRET)
+options("googleAuthR.webapp.client_id" = CLIENT_ID)
+options("googleAuthR.webapp.client_secret" = CLIENT_SECRET)
 options("googleAuthR.scopes.selected" = c("https://www.googleapis.com/auth/cloud-platform"))
 
-service_token  <- gar_auth_service(json_file = SERVICE.ACCOUNT)
+service_token  <- gar_auth_service(json_file = PATH_TO_SERVICE)
 
 
 header  <- dashboardHeader(
-	title = "Project: Picasso",
+	title = "PICASSO",
 	titleWidth = 300
 )
 
@@ -45,10 +46,16 @@ body  <- dashboardBody(
 			box(width = NULL,
 			title = "Google Cloud Vision API",
 			status = "danger",
-			p("The model takes an image and returns a dataframe of probabilities around image labels."),
+			p("The model takes an image and returns a dataframe of attributes for the given method."),
+			selectInput('method', 'Detection Method:',
+									c('Label Detection' = 'LABEL_DETECTION',
+										# TODO: Figuring out what data belongs to which face
+										'Face Detection' = 'FACE_DETECTION',
+										'Logo Detection' = 'LOGO_DETECTION',
+										'Text Detection' = 'TEXT_DETECTION',
+										'Landmark Detection' = 'LANDMARK_DETECTION')),
 			fileInput('image1', 'Select Image:',
-				accept = c(".png", ".jpg", ".jpeg"))
-			)
+				accept = c(".png", ".jpg", ".jpeg"))))
 		),
 
 		column(width = 12,
@@ -73,7 +80,13 @@ ui  <- dashboardPage(
 
 server <- function(input, output) { 
 
+	detection_method  <- reactive({
+		#	Allow users to change detection method
+		input$method
+	})
+
 	output$image  <- renderImage({
+		# Display default image, or use user supplied one
 		if (is.null(input$image1)) {
 			outfile  <- 'picasso_bull.jpeg'	
 			content_type  <- 'image/jpg'
@@ -82,28 +95,44 @@ server <- function(input, output) {
 			content_type  <- input$image1$type
 		}
 
-		list(src = outfile,
-			 contentType = content_type,
-			 width = 500)
+		# Constrain image output size, so images don't outgrow box
+		list(src = outfile, contentType = content_type, width = 500, height = 400)
 	}, deleteFile = FALSE)
 
 
 	output$image_data  <- DT::renderDataTable({
 	
 		if (is.null(input$image1)) {
+		  # Default DF to read in
 			df  <- readr::read_csv('bull_data.csv')		
 			return(DT::datatable(df))
 		} else {
-			response  <- getGoogleVisionResponse(input$image1$datapath,
-				feature = "LABEL_DETECTION", numResults = 20)
+
+			if (detection_method() == "FACE_DETECTION") {
+			# Face Detection returns A LOT of information
+			# Filtering final DF for key metrics of interest
+			response  <- getGoogleVisionResponse(input$image1$datapath, feature = "FACE_DETECTION", numResults = 20)
+
 			df_res  <- as.data.frame(response)
-			return(DT::datatable(df_res,
-						extensions = 'Buttons',
-						options = list(
-							dom = 'Bfrtip',
-							buttons = c('csv', 'excel')
-						))
-				)
+			# Filter DF for columns of interest
+			df_res  <- df_res[, c("joyLikelihood", "sorrowLikelihood",
+														"angerLikelihood", "surpriseLikelihood",
+														"underExposedLikelihood", "blurredLikelihood",
+														"headwearLikelihood")]
+
+			return(DT::datatable(df_res, extensions = 'Buttons',
+						options = list(dom = 'Bfrtip', buttons = c('csv', 'excel'))))
+
+			} else {
+
+			response  <- getGoogleVisionResponse(input$image1$datapath, feature = detection_method(), numResults = 20)
+
+			df_res  <- as.data.frame(response)
+
+			return(DT::datatable(df_res, extensions = 'Buttons',
+						options = list(dom = 'Bfrtip', buttons = c('csv', 'excel'))))
+			}
+		
 		}
 	
 	})
